0ac7df63d46ded9db72a4e0b7a002693a8fcbb5a
dpenfoldbrown <dpenfoldbrown@gmail.com>
2013-10-02 18:00:17 -0400

    Minor change to index (spacing), added publish script for pushing site to server

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Simple script to rsync web files to web servers
+
+# rsync -av <Source files> <username@host:location>
+# rysnc options:
+#   -a  archive. send files in archive mode
+#   -r  recursive. recurse into directories
+#   -v  verbose
+#   -u  update. skip files that are newer on the receiver
+#   -z  compress. compress files during transfer
+
+rsync -arvuz /Users/dpb/dev/repos/SMAPP-Website/ dpb3@wss1.bio.nyu.edu:/var/www/sites/smapp.nyu.edu --exclude '.git' --exclude 'index.html.bak' --exclude 'test.html' --exclude 'publish.sh'
